495f240f8688ffae7d26ebe5daf8a678d291eacc
Pieter <43391350+Chrysophylaxs@users.noreply.github.com>
2021-12-05 21:25:44 +0100

    Small improvements

--- day5.cpp
+++ day5.cpp
@@ -8,28 +8,28 @@ enum Direction {
 struct Point {
 	int x, y;
 
-	bool operator!=(const Point& p) { return p.x != x || p.y != y; }
-	template<Direction d> Point& next();
+	bool operator!=(const Point& p) const { return p.x != x || p.y != y; }
+	template<Direction d> Point next() const;
 };
 
 struct Line {
 	Point p1, p2;
 
 	template<Direction d> bool is() const;
-	template<Direction d> Line start_end();
+	template<Direction d> Line start_end() const;
 };
 
-template<> Point& Point::next<Horizontal>() { x++; return *this; }
-template<> Point& Point::next<Vertical>() { y++; return *this; }
-template<> Point& Point::next<Diagonal>() { x++; y++; return *this; }
-template<> Point& Point::next<AntiDiagonal>() { x--; y++; return *this; }
+template<> Point Point::next<Horizontal>() const { return {x + 1, y}; }
+template<> Point Point::next<Vertical>() const { return {x, y + 1}; }
+template<> Point Point::next<Diagonal>() const { return {x + 1, y + 1}; }
+template<> Point Point::next<AntiDiagonal>() const { return {x - 1, y + 1}; }
 
 template<> bool Line::is<Horizontal>() const { return p1.y == p2.y; }
 template<> bool Line::is<Vertical>() const { return p1.x == p2.x; }
 template<> bool Line::is<Diagonal>() const { return p1.x - p2.x == p1.y - p2.y; }
 template<> bool Line::is<AntiDiagonal>() const { return p1.x - p2.x == p2.y - p1.y; }
 
-template<Direction d> Line Line::start_end() {
+template<Direction d> Line Line::start_end() const {
 	if (p1.y < p2.y || (p1.x < p2.x && p1.y == p2.y))
 		return {p1, p2.next<d>()};
 	else
@@ -74,13 +74,13 @@ public:
 	}
 
 private:
-	void add(Point p) { ocean_floor[p.x][p.y]++; }
+	void add(const Point& p) { ocean_floor[p.x][p.y]++; }
 
 	template<Direction d>
-	void add(Line l) {
-		auto [start, end] = l.start_end<d>();
-		for (Point p = start; p != end; p.next<d>())
-			add(p);
+	void add(const Line& line) {
+		auto [start, end] = line.start_end<d>();
+		for (Point curr = start; curr != end; curr = curr.next<d>())
+			add(curr);
 	}
 
 	int ocean_floor[1000][1000] = {0};
@@ -90,24 +90,24 @@ Line lines[500];
 
 int part1() {
 	OceanFloor ocean;
-	for (int i = 0; i < 500; i++)
-		if (lines[i].is<Horizontal>() || lines[i].is<Vertical>())
-			ocean.add(lines[i]);
+	for (Line& line : lines)
+		if (line.is<Horizontal>() || line.is<Vertical>())
+			ocean.add(line);
 	return ocean.num_intersections();
 }
 
 int part2() {
 	OceanFloor ocean;
-	for (int i = 0; i < 500; i++)
-		ocean.add(lines[i]);
+	for (Line& line : lines)
+		ocean.add(line);
 	return ocean.num_intersections();
 }
 
 int main() {
 	std::ifstream input("in5.txt");
 
-	for (int i = 0; i < 500; i++)
-		input >> lines[i];
+	for (Line& line : lines)
+		input >> line;
 
 	std::cout << "Part1: " << part1() << std::endl;
 	std::cout << "Part2: " << part2() << std::endl;
